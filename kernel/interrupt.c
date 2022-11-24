ef40ac03f8b4a605e695dc858e63f56565b040ae
shop-pin <biubiubiuboom748@gmail.com>
2022-11-25 00:22:34 +0800

    11.25

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -51,8 +51,8 @@ static void pic_init(void) {
    outb (PIC_S_DATA, 0x02);    // ICW3: 设置从片连接到主片的IR2引脚
    outb (PIC_S_DATA, 0x01);    // ICW4: 8086模式, 正常EOI
    
-/* 测试键盘,只打开键盘中断，其它全部关闭 */
-   outb (PIC_M_DATA, 0xfd);
+/* 只打开时钟中断，其它全部关闭 */
+   outb (PIC_M_DATA, 0xfe);
    outb (PIC_S_DATA, 0xff);
 
    put_str("   pic_init done\n");
