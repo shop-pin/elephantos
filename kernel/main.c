184e4a896a73765a3f3613c636a4a05be1938c46
shop-pin <biubiubiuboom748@gmail.com>
2022-11-25 12:00:51 +0800

    kernel

--- kernel/main.c
+++ kernel/main.c
@@ -4,23 +4,22 @@
 #include "interrupt.h"
 #include "console.h"
 #include "process.h"
+#include "syscall-init.h"
+#include "syscall.h"
+#include "stdio.h"
+#include "memory.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
 void u_prog_a(void);
 void u_prog_b(void);
-int test_var_a = 0, test_var_b = 0;
 
 int main(void) {
    put_str("I am kernel\n");
    init_all();
-
-   thread_start("k_thread_a", 31, k_thread_a, "argA ");
-   thread_start("k_thread_b", 31, k_thread_b, "argB ");
-   process_execute(u_prog_a, "user_prog_a");
-   process_execute(u_prog_b, "user_prog_b");
-
    intr_enable();
+   thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
+   thread_start("k_thread_b", 31, k_thread_b, "I am thread_b ");
    while(1);
    return 0;
 }
@@ -28,31 +27,33 @@ int main(void) {
 /* 在线程中运行的函数 */
 void k_thread_a(void* arg) {     
    char* para = arg;
-   while(1) {
-      console_put_str(" v_a:0x");
-      console_put_int(test_var_a);
-   }
+   void* addr = sys_malloc(33);
+   console_put_str(" I am thread_a, sys_malloc(33), addr is 0x");
+   console_put_int((int)addr);
+   console_put_char('\n');
+   while(1);
 }
 
 /* 在线程中运行的函数 */
 void k_thread_b(void* arg) {     
    char* para = arg;
-   while(1) {
-      console_put_str(" v_b:0x");
-      console_put_int(test_var_b);
-   }
+   void* addr = sys_malloc(63);
+   console_put_str(" I am thread_b, sys_malloc(63), addr is 0x");
+   console_put_int((int)addr);
+   console_put_char('\n');
+   while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_a(void) {
-   while(1) {
-      test_var_a++;
-   }
+   char* name = "prog_a";
+   printf(" I am %s, my pid:%d%c", name, getpid(),'\n');
+   while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_b(void) {
-   while(1) {
-      test_var_b++;
-   }
+   char* name = "prog_b";
+   printf(" I am %s, my pid:%d%c", name, getpid(), '\n');
+   while(1);
 }
